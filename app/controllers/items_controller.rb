07415b1603267e8c3fb5e4c00f0925b3cd557d9f
Masato0530 <o.masato3536@icloud.com>
2021-06-12 14:19:09 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,7 @@ class ItemsController < ApplicationController
   
   def index
   end
-
+  
+  def items_params
+    params.require(:item).permit(content, :image).merge(user_id: current_user.id)
 end
--- db/migrate/20210612042817_create_items.rb
+++ db/migrate/20210612042817_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,               null: false
+      t.integer :price,             null: false
+      t.references :user,           null: false, forein_key: true
+      t.integer :category_id,       null: false
+      t.integer :prodact_status_id, null: false
+      t.integer :condition_id,      null: false
+      t.integer :shipping_id,       null: false
+      t.integer :description,       null: false        
       t.timestamps
     end
   end
