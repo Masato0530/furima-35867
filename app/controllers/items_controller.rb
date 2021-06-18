0afffe13476d5e0e8c1100c9c0cf753197da1196
Masato0530 <o.masato3536@icloud.com>
2021-06-18 20:06:58 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,12 +25,13 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if current_user =! @item.user
+    if current_user != @item.user
        redirect_to root_path
     end
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
